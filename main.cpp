882ee024541a62f4c8f322cd5f85a76f336450e6
Scott Hanselman <scott@hanselman.com>
2026-01-09 21:35:39 -0800

    Add Windows.Foundation.Collections.h include for WinRT JSON iteration

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <propvarutil.h>
 #include <shlobj.h>
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Data.Xml.Dom.h>
 #include <winrt/Windows.Data.Json.h>
 #include <winrt/Windows.UI.Notifications.h>
